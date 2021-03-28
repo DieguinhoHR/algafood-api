ef0f0dbfdecef19f6f968e7a6c195d268eb27882
diego <diego.rodrigues>
2021-03-28 10:35:20 -0300

    Adiciona atualização de cozinhas

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -7,7 +7,9 @@ import javax.servlet.annotation.HttpMethodConstraint;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -36,5 +38,19 @@ public class CozinhaController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cozinha salvar(@RequestBody Cozinha cozinha) {
 		return this.cozinhaRepository.save(cozinha);
+	}
+
+	@PutMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> atualizar(
+		@PathVariable Long cozinhaId,
+		@RequestBody Cozinha cozinha) {
+		Cozinha cozinhaAtual = this.cozinhaRepository.findById(cozinhaId).get();
+
+		if (cozinhaAtual != null) {
+			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+			cozinhaRepository.save(cozinhaAtual);
+			return ResponseEntity.ok(cozinhaAtual);
+		}
+		return ResponseEntity.notFound().build();
 	}
 }

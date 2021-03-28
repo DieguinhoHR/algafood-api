aef06dfd7e6c7aeed723539353aad9d4933b44b4
diego <diego.rodrigues>
2021-03-28 10:02:40 -0300

    Adiciona inserção de cozinha

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -2,9 +2,13 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.HttpMethodConstraint;
+
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,5 +30,11 @@ public class CozinhaController {
 		return cozinhaRepository.findById(cozinhaId)
 			.map(cozinha -> ResponseEntity.ok().body(cozinha))
 			.orElse(ResponseEntity.notFound().build());
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha salvar(@RequestBody Cozinha cozinha) {
+		return this.cozinhaRepository.save(cozinha);
 	}
 }
--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -11,6 +11,7 @@ import lombok.EqualsAndHashCode;
 @Entity
 public class Cozinha {
 
+	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;

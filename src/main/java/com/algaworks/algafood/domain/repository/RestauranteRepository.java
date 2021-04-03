c8944de37aa41dba1d9d93ec6f9f44066b110f43
Diego Henrique Rodrigues <diego.rodrigues@softplan.com.br>
2021-04-03 10:16:02 -0300

    Adiciona arquivo de after migrate

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -1,15 +1,21 @@
 package com.algaworks.algafood.domain.repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 
-	@Query("from Restaurante r join fetch r.cozinha join fetch r.formasPagamento")
+	@Query("from Restaurante r join fetch r.cozinha")
 	List<Restaurante> findAll();
+
+	List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+
+	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
 }
